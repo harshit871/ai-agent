3aacf25837f8e4ab68d4f1488e428332770b768f
Harshit Soni <harshitashva@gmail.com>
2026-05-05 14:29:41 +0530

    added gemini api key

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("Something went wrong during execution!")
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+usage_metadata = response.usage_metadata
+
+if usage_metadata is None:
+    raise RuntimeError("No usage metadata")
+
+print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+print(f"Response tokens: {usage_metadata.candidates_token_count}")
+print(response.text)
