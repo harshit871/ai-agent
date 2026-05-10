58376118a9852432446595ed73565cbe6a94e634
Harshit Soni <harshitashva@gmail.com>
2026-05-10 12:57:50 +0530

    feat: Verbose Output

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ client = genai.Client(api_key=api_key)
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+user_prompt = args.user_prompt
 
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents=messages
@@ -26,6 +28,8 @@ usage_metadata = response.usage_metadata
 if usage_metadata is None:
     raise RuntimeError("No usage metadata")
 
-print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
-print(f"Response tokens: {usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {usage_metadata.candidates_token_count}")
 print(response.text)
